89480a80d2b3629c045a3d579437843345cc1734
otheabould <o.theabould@googlemail.com>
2021-04-30 17:39:02 +0100

    add eslint and webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+// webpack.config.js
+const path = require('path');
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    entry: slsw.lib.entries,
+    target: 'node',
+    mode: 'production',
+    // mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+    stats: 'minimal',
+    performance: {
+        hints: false,
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+    },
+    output: {
+        libraryTarget: 'commonjs2',
+        path: path.join(__dirname, '.webpack'),
+        filename: '[name].js',
+    },
+    externals: [nodeExternals()] // exclude external modules
+};
