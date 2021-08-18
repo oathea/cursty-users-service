17413177171617d0ee5d206a5ed6573ac01154f9
otheabould <o.theabould@googlemail.com>
2021-08-18 09:44:02 +0100

    add handle remove team

---
+++ src/db/removeTeam.js
@@ -0,0 +1,23 @@
+const { dynamodb } = require('./index');
+
+module.exports = async function (id, teamID) {
+    const now = new Date().getTime();
+
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Key: {
+            id,
+        },
+        UpdateExpression: 'SET updatedAt = :updatedAt REMOVE teams.#teamID',
+        ExpressionAttributeNames: {
+            '#teamID': teamID,
+        },
+        ExpressionAttributeValues: {
+            ':updatedAt': now,
+        },
+        ReturnValues: 'UPDATED_NEW',
+    };
+
+    const updated = await dynamodb.update(params).promise();
+    return updated;
+};
--- src/handlers/handleUpdatedTeams.js
+++ src/handlers/handleUpdatedTeams.js
@@ -1,5 +1,6 @@
 const AWS = require('aws-sdk');
 const addTeam = require('../db/addTeam');
+const removeTeam = require('../db/removeTeam');
 const { teamRoles } = require('../utils/constants');
 const { useMiddleware } = require('../utils/middleware');
 
@@ -13,6 +14,10 @@ async function handleUpdatedTeams(event, context) {
         if (record.eventName === 'INSERT') {
             await handleInsert(record);
         }
+        if (record.eventName === 'REMOVE') {
+            console.log('unmarshalled record :%j', unmarshall(record));
+            await handleRemove(record);
+        }
     }
 }
 
@@ -22,7 +27,18 @@ async function handleInsert(record) {
         const userId = team.createdByUserID;
         await addTeam(userId, team.name, team.id, teamRoles.OWNER);
     } catch (err) {
-        console.log('error :%j', err);
+        console.log('handleInsert :%j', err);
+    }
+}
+
+async function handleRemove(record) {
+    try {
+        const team = unmarshall(record.dynamodb.OldImage);
+        for (const userDetails of Object.values(team.users)) {
+            await removeTeam(userDetails.id, team.id);
+        }
+    } catch (err) {
+        console.log('handleRemove :%j', err);
     }
 }
 
